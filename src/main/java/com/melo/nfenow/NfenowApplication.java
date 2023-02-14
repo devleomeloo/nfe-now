8ff246ace2e6e5758f8cdd89bc0843462c255a88
Leonardo Melo de Jesus <leomelodej@engdb.infra>
2023-02-14 06:15:58 -0300

    adjusted database config

---
+++ src/main/java/com/melo/nfenow/NfenowApplication.java
@@ -0,0 +1,13 @@
+package com.melo.nfenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfenowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfenowApplication.class, args);
+	}
+
+}
